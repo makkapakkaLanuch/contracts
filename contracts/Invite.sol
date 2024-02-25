e49cd858c1140ca765088516677db20a29e88afb
makkapakka <makkapakkagit@proton.me>
2024-02-25 13:44:33 +0800

    .

--- contracts/Invite.sol
+++ contracts/Invite.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.24;
 
 contract Invite {
+    address GUAD_CONTRACT = address(0xa82037218f45CDD6257d7F70A9b0b6e4b34e2196);
     uint256 amt_be_inviter;
     // bool ;
     // bool ;
@@ -11,6 +12,9 @@ contract Invite {
     mapping (address => bool) is_invited;
     mapping (address => address[]) inviteList;
     mapping (address => address) my_inviter;
+
+    // claim info
+    mapping (address => uint256) last_claim_block;
 
 
     function re_is_registed_inviter(address _address) public view returns(bool) {
@@ -32,4 +36,12 @@ contract Invite {
     function re_invites(address _address) public view returns(uint256) {
         return inviteList[_address].length;
     }
+
+    function re_last_claim(address _address) public view returns(uint256) {
+        return last_claim_block[_address];
+    }
+
+    // function reg_inviter() {
+
+    // }
 }
